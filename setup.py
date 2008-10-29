bb567e34dd0972ea57d3ccb1e827604617ae7770
Esteve Fernandez <esteve@fluidinfo.com>
2008-10-29 19:36:04 +0100

    bumped version to 0.2

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ from glob import glob
 
 setup(
     name = 'txAMQP',
-    version = '0.1',
+    version = '0.2',
     author = 'Esteve Fernandez',
     author_email = 'esteve@fluidinfo.com',
-    packages = ['txamqp',],
-    package_dir = { 'txamqp': 'src/txamqp',},
+    packages = ['txamqp', 'txamqp.contrib', ],
+    package_dir = {
+        'txamqp': 'src/txamqp',
+        'txamqp.contrib': 'src/txamqp/contrib',
+        },
 )
 
