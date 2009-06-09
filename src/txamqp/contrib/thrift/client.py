c7a1465f22239f46c4c73accb27f3b899efd4bb2
Terry Jones <terry@jon.es>
2009-06-09 15:29:13 +0200

    Added some logging to report on a situation I ran across wherein a msg did not have a thriftClient header. I don't know what caused this to occur and didn't try hard to reproduce it (I only saw it in a log file about 10 minutes after it had happened). So the code in this change is not even tested.

--- src/txamqp/contrib/thrift/client.py
+++ src/txamqp/contrib/thrift/client.py
@@ -7,5 +7,15 @@ class ThriftTwistedDelegate(TwistedDelegate):
 
     @defer.inlineCallbacks
     def basic_return_(self, ch, msg):
-        thriftClientName = msg.content['headers']['thriftClientName']
-        (yield self.client.thriftBasicReturnQueue(thriftClientName)).put(msg)
+        try:
+            thriftClientName = msg.content['headers']['thriftClientName']
+        except KeyError:
+            from twisted.python import log
+            if 'headers' in msg.content:
+                log.msg("'headers' not in msg.content: %r" % msg.content)
+            else:
+                log.msg("'thriftClientName' not in msg.content headers: %r" %
+                        msg.content['headers'])
+        else:
+            (yield self.client.thriftBasicReturnQueue(thriftClientName))\
+                   .put(msg)
