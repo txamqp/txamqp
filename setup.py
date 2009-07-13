bdf37d3e86ec346acde73ab544ae59941bd7a660
Zooko Wilcox-O'Hearn <zooko@zooko-desktop>
2009-07-14 09:08:24 +1200

    If setuptools is importable, use it to find the packages, and declare that txAMQP depends on Twisted.

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
-#!/usr/bin/env python
+setupdict= {
+    'name': 'txAMQP',
+    'version': '0.2',
+    'author': 'Esteve Fernandez',
+    'author_email': 'esteve@fluidinfo.com',
+    'url': 'https://launchpad.net/txamqp',
+    }
+
 try:
- # Load setuptools, to build a specific source package
- import setuptools
- setup = setuptools.setup
+    from setuptools import setup, find_packages
 except ImportError:
- from distutils.core import setup
-
-from glob import glob
-
-setup(
-    name = 'txAMQP',
-    version = '0.2',
-    author = 'Esteve Fernandez',
-    author_email = 'esteve@fluidinfo.com',
-    packages = ['txamqp', 'txamqp.contrib', 'txamqp.contrib.thrift'],
-    package_dir = {
+    from distutils.core import setup
+    setupdict['packages'] = ['txamqp', 'txamqp.contrib', 'txamqp.contrib.thrift']
+    setupdict['package_dir'] = {
         'txamqp': 'src/txamqp',
         'txamqp.contrib': 'src/txamqp/contrib',
         'txamqp.contrib.thrift': 'src/txamqp/contrib/thrift',
-        },
-    url = 'https://launchpad.net/txamqp',
-)
+    },
+else:
+    setupdict['packages'] = find_packages('src')
+    setupdict['package_dir'] = { '': 'src' }
+    setupdict['install_requires'] = ['Twisted']
+        
+setup(**setupdict)
