f06f691404bf75fceb27e925bcd52afb616ed81b
Esteve Fernandez <esteve@fluidinfo.com>
2009-08-16 13:30:01 +0200

    bump to version 0.3

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 setupdict= {
     'name': 'txAMQP',
-    'version': '0.2',
+    'version': '0.3',
     'author': 'Esteve Fernandez',
     'author_email': 'esteve@fluidinfo.com',
     'url': 'https://launchpad.net/txamqp',
